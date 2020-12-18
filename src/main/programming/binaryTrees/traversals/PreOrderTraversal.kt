38f61cca5d97b0526eb2b62ae29701f399830e1b
Vamsi Tallapudi <vamsi@coderefer.com>
2020-12-18 19:10:11 +0530

    Pre order traversal in Kotlin

--- src/main/programming/binaryTrees/traversals/PreOrderTraversal.kt
+++ src/main/programming/binaryTrees/traversals/PreOrderTraversal.kt
@@ -1,17 +1,35 @@
 package main.programming.binaryTrees.traversals
 
-class PreOrderTraversal {
+import java.util.*
 
-    fun preorderTraversal(root: TreeNode?): List<Int> {
+class PreOrderTraversal {
+    fun preorderTraversalRecursive(root: TreeNode?): List<Int> {
 //        if root is null, return list of empty array
         if (root == null) {
             return listOf()
         }
 //        iterate recursively into left child and right child
-        return listOf(root.data) + preorderTraversal(root.left) + preorderTraversal(root.right)
+        return listOf(root.data) + preorderTraversalRecursive(root.left) + preorderTraversalRecursive(root.right)
+    }
+
+    fun preOrderTraversalIterative(root:TreeNode?): List<Int> {
+        val myList = mutableListOf<Int>()
+        val stack = Stack<TreeNode>()
+        if (root == null) {
+            return myList
+        }
+        stack.add(root)
+        while (stack.isNotEmpty()) {
+            val node = stack.pop()
+            myList.add(node.data)
+            node.right?.let {stack.add(it) }
+            node.left?.let { stack.add(it) }
+        }
+        return myList
     }
 }
 
 fun main() {
-    print(PreOrderTraversal().preorderTraversal(initializeBinaryTree()))
+    print(PreOrderTraversal().preorderTraversalRecursive(initializeBinaryTree()))
+    print(PreOrderTraversal().preOrderTraversalIterative(initializeBinaryTree()))
 }
