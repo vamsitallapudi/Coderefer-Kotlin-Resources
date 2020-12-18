83e3204a5cfaa83db1c7644268131c0e5af9d9e1
Vamsi Tallapudi <vamsi@coderefer.com>
2020-12-18 20:21:08 +0530

    pre order traversal iterative function added

--- src/main/programming/binaryTrees/traversals/PreOrderTraversal.kt
+++ src/main/programming/binaryTrees/traversals/PreOrderTraversal.kt
@@ -12,17 +12,20 @@ class PreOrderTraversal {
         return listOf(root.data) + preorderTraversalRecursive(root.left) + preorderTraversalRecursive(root.right)
     }
 
-    fun preOrderTraversalIterative(root:TreeNode?): List<Int> {
+    fun preOrderTraversalIterative(root: TreeNode?): List<Int> {
         val myList = mutableListOf<Int>()
+        // creating stack to store the left and right nodes while processing root node
         val stack = Stack<TreeNode>()
+//        checking edge case and returning empty list
         if (root == null) {
             return myList
         }
-        stack.add(root)
+        stack.add(root) // adding root to stack initially
         while (stack.isNotEmpty()) {
-            val node = stack.pop()
+            val node = stack.pop() //popping each value of stack
             myList.add(node.data)
-            node.right?.let {stack.add(it) }
+            //adding right one first as its a stack and we
+            node.right?.let { stack.add(it) }
             node.left?.let { stack.add(it) }
         }
         return myList
