76f745d3e74d15101a84a5c0bc827f877ec7c941
Vamsi Tallapudi <vamsi@coderefer.com>
2020-12-22 20:11:50 +0530

    format changes

--- src/main/programming/binaryTrees/traversals/InOrderTraversal.kt
+++ src/main/programming/binaryTrees/traversals/InOrderTraversal.kt
@@ -3,22 +3,22 @@ package main.programming.binaryTrees.traversals
 import java.util.*
 
 class InOrderTraversal {
-    fun inOrderTraversalRecursive(root: TreeNode?) : List<Int> {
+    fun inOrderTraversalRecursive(root: TreeNode?): List<Int> {
         if (root == null) return emptyList()
         return inOrderTraversalRecursive(root.left) + listOf(root.data) + inOrderTraversalRecursive(root.right)
     }
 
-    fun inorderTraversalIterative(root:TreeNode?) : List<Int> {
+    fun inorderTraversalIterative(root: TreeNode?): List<Int> {
         val list = mutableListOf<Int>()
         if (root == null) return list
         var node = root
         val stack = Stack<TreeNode>()
 //        traversing the tree whenever right node is not null or the stack contains items
-        while (node !=null || stack.isNotEmpty()) {
+        while (node != null || stack.isNotEmpty()) {
 //            processing all the left nodes of the current node
             if (node != null) {
                 stack.push(node)
-                node= node.left
+                node = node.left
             } else {
                 node = stack.pop()
                 list.add(node.data) // adding to the list after all left children
