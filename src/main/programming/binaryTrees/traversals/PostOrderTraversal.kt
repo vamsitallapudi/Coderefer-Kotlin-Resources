378e7f4817d63475a0405a430e60ae8cfbb1bda1
Vamsi Tallapudi <vamsi@coderefer.com>
2020-12-23 08:32:45 +0530

    post order Iterative solution added

--- src/main/programming/binaryTrees/traversals/PostOrderTraversal.kt
+++ src/main/programming/binaryTrees/traversals/PostOrderTraversal.kt
@@ -1,13 +1,31 @@
 package main.programming.binaryTrees.traversals
 
+import java.util.ArrayDeque
+import java.util.LinkedList
+
 class PostOrderTraversal {
     fun postOrderRecursive(root: TreeNode?) : List<Int>{
         if(root == null) return emptyList()
         return postOrderRecursive(root.left) + postOrderRecursive(root.right) + listOf(root.data)
 
     }
+    fun postOrderIterative(root: TreeNode?): List<Int> {
+        if(root == null) return emptyList()
+        val stack = ArrayDeque<TreeNode>()
+        val list = LinkedList<Int>()
+        stack.push(root) // pushing the root node first
+        while (stack.isNotEmpty()) {
+            val node = stack.pop() // popping the element from stack
+            list.addFirst(node.data) // adding first element
+            node.left?.let { stack.push(it) }
+            node.right?.let { stack.push(it) }
+        }
+        return list
+    }
 }
 
 fun main() {
-    print(PostOrderTraversal().postOrderRecursive(initializeBinaryTree()))
+    val x= initializeBinaryTree()
+    print(PostOrderTraversal().postOrderRecursive(x))
+    print(PostOrderTraversal().postOrderIterative(x))
 }
